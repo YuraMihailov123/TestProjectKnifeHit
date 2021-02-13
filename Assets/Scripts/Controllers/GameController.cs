aa13a5ba276d896e8bf7e87883ae0e6031db6653
YuraMihailov <recon.97@mail.ru>
2021-02-14 00:45:31 +0300

    Initial commit

---
+++ Assets/Scripts/Controllers/GameController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+
+    #region Singleton
+    private static GameController _instance;
+
+    public static GameController Instance
+    {
+        get
+        {
+            try
+            {
+                if (_instance == null) _instance = GameObject.Find("Root").transform.Find("GameController").GetComponent<GameController>();
+            }
+            catch { }
+            return _instance;
+        }
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour
+{
+
+    #region Singleton
+    private static MenuController _instance;
+
+    public static MenuController Instance
+    {
+        get
+        {
+            try
+            {
+                if (_instance == null) _instance = GameObject.Find("Root").transform.Find("MenuController").GetComponent<MenuController>();
+            }
+            catch { }
+            return _instance;
+        }
+    }
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
